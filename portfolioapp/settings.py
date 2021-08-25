df7f04ed0924fdff3646b29af3e4ae0239eb7d5c
tomisin-isaac <talktotomison@gmail.com>
2021-08-25 14:51:36 +0100

    first commit

--- portfolioapp/settings.py
+++ portfolioapp/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'portfolioapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portdb',
+        'USER': 'portdbadmin',
+        'PASSWORD': 'missdayo123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -113,7 +123,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
